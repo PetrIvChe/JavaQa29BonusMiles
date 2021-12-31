138e782af437442f77f6fa702c10ea0f9358380e
PetrIvChe <cherniavskiipetr@gmail.com>
2021-12-31 16:20:42 +0300

    Начисляются только целые бонусные мили

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8630;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println("Вам начислено " + bonusMiles + " бонусных миль");
+
+    }
+}
